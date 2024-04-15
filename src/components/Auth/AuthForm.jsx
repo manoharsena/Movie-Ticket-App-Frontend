3292d8878d0262639262572f999f55139f3d53bc
manoharsena <manoharsena@gmail.com>
2024-04-15 18:49:25 +0530

    Updated AuthForm.jsx, Auth.jsx and Admin.jsx

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,99 +1,148 @@
-import { Box, Button, Dialog, FormLabel, IconButton, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Dialog,
+  FormLabel,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
 const labelStyle = { mt: 1, mb: 1 };
 const AuthForm = ({ onSubmit, isAdmin }) => {
-    const [inputs, setInputs] = useState({
-        name: "",
-        email: "",
-        password: "",
-      });
-      const [isSignup, setIsSignup] = useState(false);
-      const handleChange = (e) => {
-        setInputs((prevState) => ({
-          ...prevState,
-          [e.target.name]: e.target.value,
-        }));
-      };
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit({ inputs, signup: isAdmin ? false : isSignup });
-      };
-      return (
-        <Dialog PaperProps={{ style: { borderRadius: 20 } }} open={true}>
-          <Box sx={{ ml: "auto", padding: 1 }}>
-            <IconButton LinkComponent={Link} to="/">
-              <CloseRoundedIcon />
-            </IconButton>
-          </Box>
-          <Typography variant="h4" textAlign={"center"}>
-            {isSignup ? "Signup" : "Login"}
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            <Box
-              padding={6}
-              display={"flex"}
-              justifyContent={"center"}
-              flexDirection="column"
-              width={400}
-              margin="auto"
-              alignContent={"center"}
-            >
-              {!isAdmin && isSignup && (
-                <>
-                  {" "}
-                  <FormLabel sx={labelStyle}>Name</FormLabel>
-                  <TextField
-                    value={inputs.name}
-                    onChange={handleChange}
-                    margin="normal"
-                    variant="standard"
-                    type={"text"}
-                    name="name"
-                  />
-                </>
-              )}
-              <FormLabel sx={labelStyle}>Email</FormLabel>
-              <TextField
-                value={inputs.email}
-                onChange={handleChange}
-                margin="normal"
-                variant="standard"
-                type={"email"}
-                name="email"
-              />
-              <FormLabel sx={labelStyle}>Password</FormLabel>
-              <TextField
-                value={inputs.password}
-                onChange={handleChange}
-                margin="normal"
-                variant="standard"
-                type={"password"}
-                name="password"
-              />
-              <Button
-                sx={{ mt: 2, borderRadius: 10, bgcolor: "#2b2d42" }}
-                type="submit"
-                fullWidth
-                variant="contained"
-              >
-                {isSignup ? "Signup" : "Login"}
-              </Button>
-              {!isAdmin && (
-                <Button
-                  onClick={() => setIsSignup(!isSignup)}
-                  sx={{ mt: 2, borderRadius: 10 }}
-                  fullWidth
-                >
-                  Switch To {isSignup ? "Login" : "Signup"}
-                </Button>
-              )}
-            </Box>
-          </form>
-        </Dialog>
-      );
-}
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default AuthForm
+  const [isSignup, setIsSignup] = useState(false);
+  const [nameError, setUsernameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const validateForm = () => {
+    let isValid = true;
+    if (!inputs.name.trim()) {
+      setUsernameError("Username is required");
+      isValid = false;
+    } else {
+      setUsernameError("");
+    }
+    if (!inputs.email.trim()) {
+      setEmailError("Email is required");
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(inputs.email)) {
+      setEmailError("Invalid email address");
+      isValid = false;
+    } else {
+      setEmailError("");
+    }
+    if (!inputs.password.trim()) {
+      setPasswordError("Password is required");
+      isValid = false;
+    } else {
+      setPasswordError("");
+    }
+    return isValid;
+  };
+
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      onSubmit({ inputs, signup: isAdmin ? false : isSignup });
+    }
+  };
+
+  return (
+    <Dialog PaperProps={{ style: { borderRadius: 20 } }} open={true}>
+      <Box sx={{ ml: "auto", padding: 1 }}>
+        <IconButton LinkComponent={Link} to="/">
+          <CloseRoundedIcon />
+        </IconButton>
+      </Box>
+      <Typography variant="h4" textAlign={"center"}>
+        {isSignup ? "Signup" : "Login"}
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Box
+          padding={6}
+          display={"flex"}
+          justifyContent={"center"}
+          flexDirection="column"
+          width={400}
+          margin="auto"
+          alignContent={"center"}
+        >
+          {!isAdmin && isSignup && (
+            <>
+              {" "}
+              <FormLabel sx={labelStyle}>Name</FormLabel>
+              <TextField
+                value={inputs.name}
+                onChange={handleChange}
+                className={`form-control ${nameError && "is-invalid"}`}
+                margin="normal"
+                variant="standard"
+                type={"text"}
+                name="name"
+              />
+              {nameError && (<div style={{color:"red"}}>{nameError}</div>)}
+            </>
+          )}
+          <FormLabel sx={labelStyle}>Email</FormLabel>
+          <TextField
+            value={inputs.email}
+            onChange={handleChange}
+            className={`form-control ${emailError && "is-invalid"}`}
+            margin="normal"
+            variant="standard"
+            type={"email"}
+            name="email"
+          />
+          {emailError && (<div style={{color:"red"}}>{emailError}</div>)}
+          <FormLabel sx={labelStyle}>Password</FormLabel>
+          <TextField
+            value={inputs.password}
+            onChange={handleChange}
+            className={`form-control ${passwordError && "is-invalid"}`}
+            margin="normal"
+            variant="standard"
+            type={"password"}
+            name="password"
+          />
+          {passwordError && (<div style={{color:"red"}}>{passwordError}</div>)}
+          <Button
+            sx={{ mt: 2, borderRadius: 10, bgcolor: "#2b2d42" }}
+            type="submit"
+            fullWidth
+            variant="contained"
+          >
+            {isSignup ? "Signup" : "Login"}
+          </Button>
+          {!isAdmin && (
+            <Button
+              onClick={() => setIsSignup(!isSignup)}
+              sx={{ mt: 2, borderRadius: 10 }}
+              fullWidth
+            >
+              Switch To {isSignup ? "Login" : "Signup"}
+            </Button>
+          )}
+        </Box>
+      </form>
+    </Dialog>
+  );
+};
+
+export default AuthForm;
